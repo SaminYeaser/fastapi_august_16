7d929ce426fdeaa8be4b6aa5eea0b9c4676e489f
saminyeaser <samin@hishabee.io>
2024-08-16 21:10:43 -0400

    database is connected

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from fastapi import FastAPI
+from blog.schemas import Blog
+from typing import Optional
+from blog import schemas,model
+from database import SessionLocal, engine
 
 app = FastAPI()
+
+model.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get('/about')
-def about():
-    return {'data':'This is about'}
+@app.get('/blog/{id}')
+def about(id : int):
+    return { 'data' : id}
+
+@app.get('/blog/comments/{id}')
+def about(id):
+    return { 'data' : ['1','2']}
+
+@app.get('/blog')
+def about(limit = 10, published: bool = True, sort : Optional[str] = None):
+    if published:
+        return {'data': f'{limit} blogs published sort {sort}'}
+    else:
+        return {'data': f'Total {limit} blogs total sort {sort}'}
+    
+
+
+
+
+
+@app.post('/blog')
+def creatBlog(request: Blog):
+    return {'data':f'A blog created in the name of {request.title}', }
